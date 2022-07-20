4af58a2db951b4b39f469f327623c2f92d86c996
Liz Yu <qqqq929@hotmail.com>
2022-07-20 15:13:09 +0800

    add api connection to signin page

--- src/views/SignIn.vue
+++ src/views/SignIn.vue
@@ -1,13 +1,8 @@
 <template>
   <div class="container py-5">
-    <form
-      class="w-100"
-      @submit.prevent.stop="handleSubmit"
-      >
+    <form class="w-100" @submit.prevent.stop="handleSubmit">
       <div class="text-center mb-4">
-        <h1 class="h3 mb-3 font-weight-normal">
-          Sign In
-        </h1>
+        <h1 class="h3 mb-3 font-weight-normal">Sign In</h1>
       </div>
 
       <div class="form-label-group mb-2">
@@ -22,7 +17,7 @@
           autocomplete="username"
           required
           autofocus
-        >
+        />
       </div>
 
       <div class="form-label-group mb-3">
@@ -36,10 +31,11 @@
           placeholder="Password"
           autocomplete="current-password"
           required
-        >
+        />
       </div>
 
       <button
+        :disabled="isProcessing"
         class="btn btn-lg btn-primary btn-block mb-3"
         type="submit"
       >
@@ -48,36 +44,66 @@
 
       <div class="text-center mb-3">
         <p>
-          <router-link to="/signup">
-            Sign Up
-          </router-link>
+          <router-link to="/signup"> Sign Up </router-link>
         </p>
       </div>
 
-      <p class="mt-5 mb-3 text-muted text-center">
-        &copy; 2017-2018
-      </p>
+      <p class="mt-5 mb-3 text-muted text-center">&copy; 2017-2018</p>
     </form>
   </div>
 </template>
 
 <script>
+import authorizationAPI from '../apis/authorization';
+import { Toast } from '../utils/helpers';
+
 export default {
   data() {
     return {
       email: '',
       password: '',
-    }
+      isProcessing: false,
+    };
   },
   methods: {
-    handleSubmit() {
-      const data = JSON.stringify({
-        email: this.email,
-        password: this.password
-      })
+    async handleSubmit() {
+      try {
+        if (!this.email || !this.password) {
+          Toast.fire({
+            icon: 'warning',
+            title: '請填入 email 和 password',
+          });
+          return;
+        }
 
-      console.log(data)
-    }
-  }
-}
+        this.isProcessing = true;
+        const response = await authorizationAPI.signIn({
+          email: this.email,
+          password: this.password,
+        });
+
+        // 取得 API 請求後的資料
+        const { data } = response;
+
+        if (data.status !== 'success') {
+          throw new Error(data.message);
+        }
+
+        // 將 token 存放在 localStorage 內
+        localStorage.setItem('token', data.token);
+
+        // 成功登入後轉址到餐廳首頁
+        this.$router.push('/restaurants');
+      } catch (error) {
+        Toast.fire({
+          icon: 'warning',
+          title: '請確認您輸入了正確的帳號密碼',
+        });
+
+        this.isProcessing = false;
+        console.log('error', error);
+      }
+    },
+  },
+};
 </script>
